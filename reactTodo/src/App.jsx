4ffe1a9ffe89fda40a6c3ff0fea2e725b11aea43
Sanjay-799 <sanjaydasari799@gmail.com>
2025-06-04 15:46:51 +0530

    added styling to input and ul

--- reactTodo/src/App.jsx
+++ reactTodo/src/App.jsx
@@ -10,12 +10,24 @@ function App() {
       <div className='todo-list-mainDiv '>
         <h3>My Todo List</h3>
         <div>
-          <input type='text' name='todo-item-input' placeholder='Enter Your Todo Task'/>
-          <buttom className="btn btn-danger btn-sm">+</buttom>
-          <ul>
-            <li>
-              
+          <div className='todo-task-input-div'>
+            <div class="form-floating w-75">
+              <input type="text" className="form-control" id="floatingInput" placeholder="Todo task" />
+              <label htmlFor="floatingInput">Todo Task</label>
+            </div>
+            <buttom className="btn btn-primary" id='add-button'>+</buttom>
+          </div>
+          
+          
+          <ul className='task-list'>
+            <li className='task d-flex justify-content-between'>Writing Notes
+              <div className='task-buttons w-25'>
+                <button className='btn btn-sm btn-success'>Edit</button>
+                <button className='btn btn-sm btn-danger'>Delete</button>
+              </div>
             </li>
+            <li>Reading Book</li>
+            <li>Attending Event</li>
           </ul>
         </div>
 
